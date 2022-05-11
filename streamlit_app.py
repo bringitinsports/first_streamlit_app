daa0595b0974b420f336751306882c5fe88dce0b
bringitinsports <105378026+bringitinsports@users.noreply.github.com>
2022-05-11 12:25:51 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.text('😜Omega 3 & Blueberry Oatmeal')
 streamlit.text('🎉Kale, Spinach & Rocket Smoothie')
 streamlit.text('🙌Hard-Boiled Free-Range Egg')
 streamlit.text('💋🌹 Avocado Toast')
+
+streamlit.header('🎂🤳 Build Your Own Fruit Smoothie 🤦‍♀️🤷‍♀️')
