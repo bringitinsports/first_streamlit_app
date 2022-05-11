b60de9703b44dd06f79ac0843540d54de681fc5b
bringitinsports <105378026+bringitinsports@users.noreply.github.com>
2022-05-11 12:29:34 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,7 @@ streamlit.text('🙌Hard-Boiled Free-Range Egg')
 streamlit.text('💋🌹 Avocado Toast')
 
 streamlit.header('🎂🤳 Build Your Own Fruit Smoothie 🤦‍♀️🤷‍♀️')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
