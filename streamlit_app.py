c9ae0865fac930ab89aeeb2db6937ee609fc69ea
bringitinsports <105378026+bringitinsports@users.noreply.github.com>
2022-05-19 15:43:44 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,8 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section to display fruityvice api response
 streamlit.header('🎂🤳 Fruity Fruit Advice! 🤦‍♀️🤷‍♀️')
+fruit_choice = streamlist.text_input('What fruit would you like information about?', 'Kiwi')
+streamlist.write('The user entered', fruit_choice)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +"kiwi")
